33ce24c882c7c7e4d9662a49be5d164792e822e9
nem <lune213@gmail.com>
2021-10-24 19:32:28 +0900

    277page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
 const App = () => {
+
+  const [todos, setTodos] = useState ([
+    {
+      id: 1,
+      text: '할일 첫번째',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '할일 두번째',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '할일 세번째',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert/>
+      <TodoList todos = {todos} />
     </TodoTemplate>
   );
   
